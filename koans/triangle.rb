2b7c8730e51a655bee4485fbb78f584b50a939e6
Bhargavi Kurukunda <bhargavi.kurukunda@students.iiit.ac.in>
2024-10-10 22:55:47 -0700

    172/284 (60%) done.

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,12 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  raise TriangleError, 'Sum of two sides must be greater than the third.' if a + b < c || b + c < a || a + c < b # not a triangle!
+  raise TriangleError, 'Sum of two sides must not be equal to the third.' if a + b == c || b + c == a || a + c == b # degenerate triangle!
+
+  return :equilateral  if a == b && a == c
+  return :isosceles    if a == b || b == c || a == c
+  return :scalene      if a != b && b != c && a != c
 end
 
 # Error class used in part 2.  No need to change this code.
